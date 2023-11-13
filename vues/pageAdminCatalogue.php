43d3b20b076c5637243458f214cdde09129555fa
Olivier Raymond <olivier.raymond176@gmail.com>
2023-11-13 11:45:51 -0500

    ajouter la référence a JQuery

--- vues/pageAdminCatalogue.php
+++ vues/pageAdminCatalogue.php
@@ -24,7 +24,6 @@ if ($typeActeur == "admin")
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
     <link rel="stylesheet" href="assets/css/style_admin.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-
     <script type="text/javascript" src="assets/js/script.js"></script>
 </head>
 
