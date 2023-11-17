911bbbdc78f72e16d50fd464e7a08c73f890e69b
Olivier Raymond <olivier.raymond176@gmail.com>
2023-11-17 11:48:37 -0500

    Commenter le code

--- vues/pageEntrerOeuvre.php
+++ vues/pageEntrerOeuvre.php
@@ -25,7 +25,7 @@ if (!isset($_SESSION['errPrix'])) {
     $_SESSION['errPrix'] = "";
 }
 
-//Réinitialisation des variables globale associées aux propriétés de la nouvelle oeuvre
+//Réinitialisation des variables globales associées aux propriétés de la nouvelle oeuvre
 $titreOeuvre = "";
 $descOeuvre = "";
 $prix = "";
@@ -51,15 +51,13 @@ if (isset($_SESSION['photo'])) {
 ?>
 
 <head>
-    <!--
-    Inclusion de
-        - Bootstrap
-        - Font-awesome (icone de panier)
-    -->
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $titre ?></title>
+    
+    <!--Inclusions-->
+    <!--Bootstrap-->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css"/>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js"
             integrity="sha384-ODmDIVzN+pFdexxHEHFBQH3/9/vQ9uori45z4JjnFsRydbmQbmL5t1tQ0culUzyK"
@@ -67,7 +65,9 @@ if (isset($_SESSION['photo'])) {
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa"
             crossorigin="anonymous"></script>
+    <!--Font awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <!--JS et CSS maison-->
     <link rel="stylesheet" href="assets/css/style_admin.css">
     <script type="text/javascript" src="assets/js/script.js"></script>
 </head>
@@ -137,7 +137,7 @@ if (isset($_SESSION['photo'])) {
                                         </div>
                                         <br>
                                     </div>
-                                    
+
                                     <div class = "etape">
                                         <!-- résumé de l’ajout-->
                                         <input type="submit" value="Ajouter">
