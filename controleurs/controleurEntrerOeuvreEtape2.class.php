dfd100aeefa0af06ffee178f9ffb84113ad9e5fc
Olivier Raymond <olivier.raymond176@gmail.com>
2023-11-21 14:21:55 -0500

    déplacer la validation d’entrée dans le controleur

--- controleurs/controleurEntrerOeuvreEtape2.class.php
+++ controleurs/controleurEntrerOeuvreEtape2.class.php
@@ -10,6 +10,40 @@ class EntrerOeuvreEtape2 extends Controleur
 
     public function executerAction()
     {
-        return "pageEntrerOeuvreEtape2";
+        $this->validerEtape1();
+        if (empty($_SESSION['titre'])) {
+            return "pageEntrerOeuvre";
+        } else {
+            return "pageEntrerOeuvreEtape2";
+        }
+        
+    }
+    public function validerEtape1(){
+         
+        /**
+         * Cette fonction prend un paramètre en entrée et le débarasse des espaces et slashes inutiles
+         * elle évite l’injection de code malicieux en remplaçant les charactères spéciaux par des entités HTML
+         * ref: https://www.w3schools.com/php/php_form_validation.asp
+         * De plus, la fonction enregistre des messages d’erreurs dans le tableau de variables globales si l’un
+         * des champs nécessaire est manquant.
+         * @param $data
+         * @return string
+         */
+
+        $_SESSION['titre'] = "";
+        $_SESSION['descOeuvre'] = "";
+
+        $errTitre = "";
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            if (empty($_POST["titre"])) {
+                $errTitre = "Le Titre doit être fourni";
+            } else {
+                $_SESSION['titre'] = testerEntree($_POST['titre']);
+            }
+            if (!empty($_POST["desc_oeuvre"])) {
+                $_SESSION['descOeuvre'] = testerEntree($_POST["desc_oeuvre"]);
+            }
+            $_SESSION['errTitre'] = $errTitre;   
+        }
     }
 }
