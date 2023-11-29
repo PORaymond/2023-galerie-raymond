caf8ff00670a68d156c9901a3665693cb5de5b33
Olivier Raymond <olivier.raymond176@gmail.com>
2023-11-29 09:27:17 -0500

    Changer les couleurs de la mise en page

--- vues/vueLoginAdmin.php
+++ vues/vueLoginAdmin.php
@@ -20,20 +20,20 @@ if (session_status() == PHP_SESSION_NONE)
     <div class="container py-5 h-100">
         <div class="row d-flex justify-content-center align-items-center h-100">
             <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-                <div class="card bg-dark text-white" style="border-radius: 1rem;">
+                <div class="card couleur-fond-clair text-fonce" style="border-radius: 1rem;">
                     <div class="card-body p-5 text-center">
                         <div class="mb-md-5 mt-md-4 pb-5">
 
                             <h2 class="fw-bold mb-2">Connexion</h2>
 
-                            <p class="text-white-50 mb-5">Administrateur</p>
+                            <p class="text-fonce mb-5">Administrateur</p>
                             
                             <form action="?action=seConnecterAdmin" method="post" class="form-outline mb-4">
                                 <input type="text" id="nom_admin" name="nom_admin"
                                        placeholder="Nom d'utilisateur" class="form-control form-control-lg text-center"><br>
                                 <input type="password" name="mot_passe" placeholder="Mot de passe"
                                        class="form-control form-control-lg text-center"><br>
-                                <button class="btn btn-outline-light btn-lg px-5" type="submit">Entrer</button>
+                                <button class="btn btn-outline-light btn-lg px-5 text-fonce contour-fonce" type="submit">Entrer</button>
                                 <p><?php include_once "vues/inclusions/fonctions.inc.php";
                                     afficherErreurs($controleur->getMessagesErreur());?>
                                 </p>
